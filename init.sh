a9ce11dd9109b4a68a2c1a3cae49d516155c42be
nick <notetrrael@mas.sd>
2019-10-06 16:25:02 -0400

    Trigger ssh tunnel interface addressing of server.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./tunmake.sh
-ssh -o "StrictHostKeyChecking no" -w 1:1 -i /root/.ssh/key.pem root@$TARGET_HOST -p 8022 &
+ssh -o "StrictHostKeyChecking no" -w 1:1 -i /root/.ssh/key.pem root@$TARGET_HOST -p 8022 '/app/connected.sh' &
 sleep 5
 ifconfig tun1 up
-ifconfig tun1 10.13.37.1 netmask 255.255.255.0
+ifconfig tun1 10.13.37.1 netmask 255.255.255.252
 sleep 4200000
